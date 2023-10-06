914eff6b4ac8d9c4b3200d73d98efd15da8ed591
The-renaissance-man <mutumabrianm@gmail.com>
2023-10-06 13:50:58 +0300

    Init

--- backend/app.py
+++ backend/app.py
@@ -4,7 +4,7 @@ from flask_cors import CORS
 import requests
 
 app = Flask(__name__)
-CORS(app,resources={r"/*": {"origins": "https://botpredict.vercel.app/"}})
+CORS(app,resources={r"/*": {"origins": "https://botpredict.vercel.app"}})
 
 @app.route('/predict', methods=['POST'])
 def predict():
